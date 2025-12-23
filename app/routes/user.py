504ba41915903e2922f2dee84f424d731d76c7a2
Sagar Negi <sagarnegi926@gmail.com>
2025-12-23 20:08:16 +0530

    feat: user registration with hashing and migration

--- app/routes/user.py
+++ app/routes/user.py
@@ -3,6 +3,7 @@ from app.schemas.user import UserCreate, UserResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db import get_db_session
 from app.crud.user import create_user, get_user_by_email
+from app.core.security import hash_password
 
 
 router = APIRouter(prefix="/users", tags=["Users Register"])
@@ -15,7 +16,7 @@ async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db_sess
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="email already registered")
     
-    hashed_password = "abcd-vaha-se-nikle-pandaji"
+    hashed_password = hash_password(user.password)
 
     new_user = await create_user(db, email, hashed_password)
 
